296ef24e0cd28bd290ea781f4c499fa5de8a55f4
killerchipy723 <diegoeduardo66@gmail.com>
2025-10-08 15:45:17 -0300

    ok realizada pagina web

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask,render_template,redirect,url_for,request,flash
 
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("/sitio/index.html")
+
+#Seccion de Libros
+@app.route("/libros")
+def libros():
+    return render_template("/sitio/libros.html")
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
 
 
