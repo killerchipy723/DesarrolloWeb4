89b281a3557035d5c875a25d1f82ac136568abad
Diego Alderete <141648675+killerchipy723@users.noreply.github.com>
2025-11-18 14:14:36 -0300

    Update database connection parameters

--- db.py
+++ db.py
@@ -2,10 +2,10 @@ import pymysql
 
 def conn():
     conexion = pymysql.connect(
-        host='localhost',
-        user='root',
-        passwd='',
-        database='ies6021'
+        host='200.58.106.156',
+        user='c2710325_killer',
+        passwd='SistemaIES6021',
+        database='c2710325_sistema'
     )
     print('Conexion Exitosa')
     return conexion
