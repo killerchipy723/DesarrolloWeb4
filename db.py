724be0a898d559a56d04644d7ce3b4ca695af198
Diego Alderete <141648675+killerchipy723@users.noreply.github.com>
2025-11-19 10:53:43 -0300

    Change database name in db.py connection

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ def conn():
         host='200.58.106.156',
         user='c2710325_killer',
         passwd='SistemaIES6021',
-        database='c2710325_sistema'
+        database='c2710325_admin'
     )
     print('Conexion Exitosa')
     return conexion
